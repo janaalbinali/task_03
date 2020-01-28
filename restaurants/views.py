703ce7747629eca29398e30494820129241bcc3d
Jana Bin Ail <jwalbinali@gmail.com>
2020-01-28 11:08:09 +0300

    taskdone

--- restaurants/views.py
+++ restaurants/views.py
@@ -5,15 +5,34 @@ def welcome(request):
 
 def restaurant_list(request):
 
-    context = {
-
-    }
+    context = { 
+        "my_list":[
+            {
+                "restaurant_name": "Johnny Rockets",
+                "food_type": "Burgers and Fries",
+            },
+            {
+                "restaurant_name": "Nino",
+                "food_type": "Pastas, burgers and fries",
+            },
+            {
+                "restaurant_name": "Maki",
+                "food_type": "Japanese",
+            }
+        ]
+        }
     return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
 
     context = {
-
+        "my_object":
+        {
+            "restaurant_name": "Johnny Rockets",
+            "food_type" : "Burgers and Fries",
+        }
     }
     return render(request, 'detail.html', context)
+
+
